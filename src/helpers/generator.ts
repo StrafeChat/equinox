f3b694470d0a931a98a2b46699fb1e5d339e8576
RT <rustytrim11@gmail.com>
2023-12-24 02:29:42 -0500

    Random string generation

--- src/helpers/generator.ts
+++ src/helpers/generator.ts
@@ -1,8 +1,26 @@
 import { Snowflake } from "../util/Snowflake"
+import crypto from "crypto";
 
 const snowflakes = new Map<number, Snowflake>();
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 export const generateSnowflake = (workerId: number) => {
     if (!snowflakes.has(workerId)) snowflakes.set(workerId, new Snowflake(parseInt(process.env.SNOWFLAKE_EPOCH!, 10), workerId));
     return snowflakes.get(workerId)!.generate();
+}
+
+export const generateRandomString = (length = 12) => {
+    const randomBytes = crypto.randomBytes(length);
+    let key = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = randomBytes[i] % chars.length;
+        key += chars.charAt(randomIndex);
+    }
+
+    return key;
+}
+
+export const generateToken = (id: string, last_pass_reset: number, secret: string) => {
+
 }
