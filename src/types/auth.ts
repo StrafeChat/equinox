1e8d6c98fd94b50b3baf8986a954bcbab65d0b89
RT <rustytrim11@gmail.com>
2023-12-24 00:07:56 -0500

    Interface for register body

---
+++ src/types/auth.ts
@@ -0,0 +1,9 @@
+export interface RegisterBody {
+    email: string;
+    global_name: string;
+    username: string;
+    discriminator: number;
+    password: string;
+    dob: Date;
+    locale: string;
+};
