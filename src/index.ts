5a3d06069660d964deee6fa503c3cfcd50c91805
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-02-03 23:02:22 -0600

    Fix

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ app.set('trust proxy', 1);
 app.disable('x-powered-by');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Headers', "*");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     if (req.method === 'OPTIONS') {
         res.status(200).send();
     } else {
