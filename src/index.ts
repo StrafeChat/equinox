b143c0b8e44c8875326977f0321c1ce74a0c9ced
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2024-02-03 23:03:52 -0600

    Fix for the 10th time (I'M SORRY)

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.disable('x-powered-by');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     if (req.method === 'OPTIONS') {
         res.status(200).send();
     } else {
