3aa4d60e54f74763ece0081638f76bf0c5329568
star_sailor <71993095+Cinnamonsroll@users.noreply.github.com>
2025-04-18 14:16:07 -0700

    Improve status.go
    - Removes the need for a global map
    
    - Slightly faster than map lookup (compiler optimizes switch on strings efficiently)
    
    - Immutable by design

--- src/utils/status.go
+++ src/utils/status.go
@@ -1,15 +1,10 @@
 package utils
-
-// Valid status values
-var validStatuses = map[string]bool{
-	"online":    true,
-	"idle":      true,
-	"dnd":       true,
-	"offline":   true,
-}
-
 // IsValidStatus checks if the given status is valid
 func IsValidStatus(status string) bool {
-	_, exists := validStatuses[status]
-	return exists
+	switch status {
+	case "online", "idle", "dnd", "offline":
+		return true
+	default:
+		return false
+	}
 }
