23c65d6403927e7892d4df5de4f12e3b76c5323f
Bryden Calaway <67612593+BrydenIsNotSmart@users.noreply.github.com>
2025-07-09 16:11:33 -0500

    try fix shadow

--- src/database/models/Session.go
+++ src/database/models/Session.go
@@ -35,6 +35,5 @@ func (u *Session) SchemaDefinition() []string {
 			created_at timestamp,
 			expires_at timestamp
 		);`,
-		`CREATE INDEX IF NOT EXISTS sessions_user_id_idx ON sessions (user_id);`,
 	}
 }
